9b4b78bca9c775540f3e3be1ad50ce758ae2dc62
wontop02 <wjy0502kr@naver.com>
2025-04-06 02:28:46 +0900

    [FEAT] add post, comment api

--- Likelion-Django/likelion-13th/comments/views.py
+++ Likelion-Django/likelion-13th/comments/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse 
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_http_methods 
+from .models import *
+import json
 
-# Create your views here.
+@require_http_methods(["GET"])
+def comment_list(request, post_id):
+
+    comments_of_post = Comment.objects.all().filter(post__id = post_id) 
+    # 외부 모델의 필드를 사용할 때, 조건을 사용한 경우에는 언더바 2개 __ 사용. 
+    # 현재는 comment가 가진 post의, id를 사용하기 때문에 외부 필드
+    comments_of_post_json = []
+
+    for comment in comments_of_post:
+        comment_json = {
+            "id": comment.id,
+            "writer": comment.writer,
+            "content": comment.content,
+            "post": comment.post.id
+        }
+        comments_of_post_json.append(comment_json)
+
+    return JsonResponse({
+        "status": 200,
+        "message": "댓글 목록 조회 성공",
+        "data": comments_of_post_json
+    })
--- Likelion-Django/likelion-13th/posts/urls.py
+++ Likelion-Django/likelion-13th/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from posts.views import *
+from comments.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('<int:id>', get_post_detail) # 추가
+
+    path('', post_list, name="post_list"),
+    path('category/<int:category_id>/', post_list_category, name="post_list_category"),
+    path('<int:post_id>/', post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', comment_list, name='comment_list'),
 ]
